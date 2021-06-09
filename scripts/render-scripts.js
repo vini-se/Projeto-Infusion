edb1a3c2875846470efb9bf04debec07750aef5b
Vinicius de Lima Albino <vinicius.dla2106@gmail.com>
2021-06-09 00:36:32 -0300

    Scripts para o /dist

---
+++ scripts/render-scripts.js
@@ -0,0 +1,16 @@
+'use strict';
+const fs = require('fs');
+const upath = require('upath');
+const sh = require('shelljs');
+
+module.exports = function renderScripts() {
+    const sourcePath = upath.resolve(upath.dirname(__filename), '../src/js/main.js');
+    const destPath = upath.resolve(upath.dirname(__filename), '../dist/js/main.js');
+
+    const destPathDirname = upath.dirname(destPath);
+    // if (!sh.test('-e', destPathDirname)) {
+        sh.mkdir('-p', destPathDirname);
+    // }
+
+    sh.cp('-R', sourcePath, destPath);
+};
---
+++ scripts/sb-watch.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const _ = require('lodash');
+const chokidar = require('chokidar');
+const upath = require('upath');
+const renderAssets = require('./render-assets');
+const renderHTML = require('./render-html');
+// const renderPug = require('./render-pug');
+const renderSCSS = require('./render-scss');
+
+const watcher = chokidar.watch('src', {
+    persistent: true,
+});
+
+let READY = false;
+
+process.title = 'pug-watch';
+process.stdout.write('Loading');
+// let allPugFiles = {};
+
+watcher.on('add', filePath => _processFile(upath.normalize(filePath), 'add'));
+watcher.on('change', filePath => _processFile(upath.normalize(filePath), 'change'));
+watcher.on('ready', () => {
+    READY = true;
+    console.log(' READY TO ROLL!');
+});
+
+_handleSCSS();
+
+function _processFile(filePath, watchEvent) {
+    
+    // if (!READY) {
+    //     if (filePath.match(/\.pug$/)) {
+    //         if (!filePath.match(/includes/) && !filePath.match(/mixins/) && !filePath.match(/\/pug\/layouts\//)) {
+    //             allPugFiles[filePath] = true;
+    //         }    
+    //     }    
+    //     process.stdout.write('.');
+    //     return;
+    // }
+
+    console.log(`### INFO: File event: ${watchEvent}: ${filePath}`);
+
+    if (filePath.match(/src\/.html\//)) {
+        return renderHTML();
+    }
+
+    if (filePath.match(/\.scss$/)) {
+        if (watchEvent === 'change') {
+            return _handleSCSS(filePath, watchEvent);
+        }
+        return;
+    }
+
+    if (filePath.match(/src\/assets\//)) {
+        return renderAssets();
+    }
+
+}
+
+// function _handlePug(filePath, watchEvent) {
+//     if (watchEvent === 'change') {
+//         if (filePath.match(/includes/) || filePath.match(/mixins/) || filePath.match(/\/pug\/layouts\//)) {
+//             return _renderAllPug();
+//         }
+//         return renderPug(filePath);
+//     }
+//     if (!filePath.match(/includes/) && !filePath.match(/mixins/) && !filePath.match(/\/pug\/layouts\//)) {
+//         return renderPug(filePath);
+//     }
+// }
+
+// function _renderAllPug() {
+//     console.log('### INFO: Rendering All');
+//     _.each(allPugFiles, (value, filePath) => {
+//         renderPug(filePath);
+//     });
+// }
+
+function _handleSCSS() {
+    renderSCSS();
+}
