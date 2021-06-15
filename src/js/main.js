1178c1e2035439eabe678fd6d1c7cad33db9ce3c
Vinicius de Lima Albino <vinicius.dla2106@gmail.com>
2021-06-15 10:04:02 -0300

    Hambúrguer finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-alert('hello');
+var $btn_hmbg = document.querySelector('.menu-btn');
+var $nav = document.getElementById('nav');
+
+$btn_hmbg.addEventListener('click', () => {
+    $nav.classList.toggle('open');
+    $btn_hmbg.classList.toggle('open');
+})
+    
+
